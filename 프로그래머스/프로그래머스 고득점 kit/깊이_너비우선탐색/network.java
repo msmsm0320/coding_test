6f1db78a57c4487b9e9fecc503ba0e8ed1b3cc64
msmsm0320 <ms000320@naver.com>
2025-01-19 18:28:47 +0900

    add:new code

---
+++ 프로그래머스/프로그래머스 고득점 kit/깊이_너비우선탐색/network.java
@@ -0,0 +1,40 @@
+package 깊이_너비우선탐색;
+
+import java.util.*;
+
+
+class Solution {
+
+    static boolean visited[];
+    static int answer;
+
+    public void bfs(int[][] computers,int n, int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()){
+            int point = q.poll();
+            System.out.println(point);
+            visited[point] = true;
+            for(int i = 0; i<n; i++){
+                if(visited[i]) continue;
+                if(computers[point][i] == 1){
+                    q.offer(i);
+                }
+            }
+        }
+        answer++;
+    }
+
+    public int solution(int n, int[][] computers) {
+        answer = 0;
+        visited=new boolean[n];
+
+        for(int i = 0; i<visited.length; i++){
+            if(visited[i]) continue;
+            bfs(computers,n,i);
+        }
+        return answer;
+    }
+}
---
+++ 프로그래머스/프로그래머스 고득점 kit/깊이_너비우선탐색/target_number.java
@@ -0,0 +1,34 @@
+package 깊이_너비우선탐색;
+
+import java.util.*;
+
+
+class Solution {
+
+    static  int answer;
+    static int  count;
+    public void dfs(int[] numbers, int target, int check, int depth){
+        if(check == target && depth == numbers.length){
+            answer++;
+            return;
+        }
+
+        if(depth == numbers.length){
+            return;
+        }
+
+        int check_p = check + numbers[depth];
+        int check_n = check - numbers[depth];
+
+        dfs(numbers, target, check_p, depth + 1);
+        dfs(numbers, target, check_n, depth + 1);
+
+    }
+
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+}
