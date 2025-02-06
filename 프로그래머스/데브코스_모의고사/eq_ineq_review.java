3dc58a7c3480b2ab41dc34e0ebaef4af170b23da
msmsm0320 <ms000320@naver.com>
2025-02-06 17:16:55 +0900

    add:데브코스_모의고사

--- 프로그래머스/데브코스_모의고사/eq_ineq_review.java
+++ 프로그래머스/데브코스_모의고사/eq_ineq_review.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
-public class eq_ineq_review {
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        if(ineq.equals("<") && eq.equals("=")){
+            if(n<=m) answer = 1;
+            else answer = 0;
+        } else if(ineq.equals("<") && eq.equals("!")){
+            if(n<m) answer = 1;
+            else answer = 0;
+        } else{
+            if(eq.equals("=")){
+                if(n>=m) answer = 1;
+                else answer = 0;
+            }else{
+                if(n>m) answer =1;
+                else answer =0;
+            }
+        }
+        return answer;
+    }
 }
--- 프로그래머스/데브코스_모의고사/upper_lower_review.java
+++ 프로그래머스/데브코스_모의고사/upper_lower_review.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        for(int i =0; i<s.length(); i++){
+            char c = s.charAt(i);
 
-public class upper_lower_review {
+            if(c == ' '){
+                sb.append(c);
+                index = 0;
+            }else{
+                if(index%2 == 0){
+                    sb.append(Character.toUpperCase(c));
+                }else{
+                    sb.append(Character.toLowerCase(c));
+                }
+                index++;
+            }
+        }
+        return sb.toString();
+    }
 }
