7cf6eca123166538a16e2a9415a3569afe9ee769
msmsm0320 <ms000320@naver.com>
2025-02-10 22:12:04 +0900

    add:백트래킹

--- 백준문제/단계별로 풀어보기/백트래킹/BOJ_15649_N과_M_1_review.java
+++ 백준문제/단계별로 풀어보기/백트래킹/BOJ_15649_N과_M_1_review.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class BOJ_15649_N과_M_1_review {
+
+    static int permute[];
+    static boolean visited[];
+
+    public static void dfs(int depth, int length){
+        if(depth == length){
+            for(int k : permute){
+                System.out.print(k + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 1; i<visited.length; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            permute[depth] =i;
+            dfs(depth+1,length);
+            visited[i] = false;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N+1];
+        permute = new int[M];
+
+        dfs(0,M);
+
+        br.close();
+    }
 }
--- 백준문제/단계별로 풀어보기/백트래킹/BOJ_9663_N_Queen_review.java
+++ 백준문제/단계별로 풀어보기/백트래킹/BOJ_9663_N_Queen_review.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class BOJ_9663_N_Queen_review {
+    static int count; // 퀸을 놓을 수 있는 총 갯수
+    static int[] chess; // x번째 행, y번째 열에 퀸이 있음을 표시
+
+    // dfs 접근
+    public static void dfs(int depth){
+        if(depth == chess.length){ // 모든 행에 퀸을 두면 dfs return
+            count++;
+            return;
+        }
+        // 반복문 통해서 depth 행에 여러 열을 둬서 경우의 수 확인
+        for(int i = 0; i<chess.length; i++){
+            chess[depth] = i;
+            if(possible(depth)){ // 해당 depth 행에 i열에 놓은 것이 가능하면 depth+1 로 넘어감
+                dfs(depth+1);
+            }
+        }
+    }
+
+    public static boolean possible(int depth){
+
+        for(int i = 0; i<depth; i++){
+            if(chess[depth] == chess[i] || Math.abs(depth - i) == Math.abs(chess[depth] - chess[i])) return false;// 이전까지의 행에 같은 열에 퀸이 있는지 확인, 대각선 위치에 퀸이 있는지 확인
+        }
+        return true;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        count = 0;
+        chess = new int[N];
+
+        dfs(0);
+
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+        br.close();
+
+
+    }
 }
