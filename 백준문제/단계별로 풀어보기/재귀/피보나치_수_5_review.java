d9a991703cdceffbd1acde38905dae9a1c80c2c9
msmsm0320 <ms000320@naver.com>
2025-02-14 18:24:21 +0900

    add:재귀

---
+++ 백준문제/단계별로 풀어보기/재귀/피보나치_수_5_review.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class 피보나치_수_5_review {
+    public static int fib(int n){
+        if(n == 0){
+            return 0;
+        }else if(n == 1) {
+            return 1;
+        }else{return fib(n-1) + fib(n-2);
+    }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        int result = fib(N);
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
