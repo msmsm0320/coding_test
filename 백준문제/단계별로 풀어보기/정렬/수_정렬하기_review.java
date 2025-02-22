8ec5aa214ef922d058a7292f0457d13cb3275b98
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-02-22 15:20:59 +0900

    add: 정렬

---
+++ 백준문제/단계별로 풀어보기/정렬/수_정렬하기_review.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class 수_정렬하기_review {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for(int i = 0; i<N ; i++){
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+        }
+
+        Arrays.sort(arr);
+
+        for(int k : arr){
+            bw.write(k + "\n");
+            bw.flush();
+        }
+        bw.close();
+        br.close();
+    }
+}
