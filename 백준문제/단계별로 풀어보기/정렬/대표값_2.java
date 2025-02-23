98a000ced059a3aab18752108e8fa2a187c14cf2
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-02-23 20:49:16 +0900

    Create 대표값_2.java

---
+++ 백준문제/단계별로 풀어보기/정렬/대표값_2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class 대표값_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[5];
+        int sum = 0;
+        for(int i = 0; i<5; i++){
+            int n = Integer.parseInt(br.readLine());
+            arr[i] = n;
+            sum+=n;
+        }
+
+        sum/=5;
+        Arrays.sort(arr);
+
+        bw.write(sum + "\n");
+        bw.write(String.valueOf(arr[2]));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
