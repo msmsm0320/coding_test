b40865031c0e3822804cfed80203db80b02bda92
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-02-26 15:31:44 +0900

    add: 정렬

---
+++ 백준문제/단계별로 풀어보기/정렬/소트인사이드_review.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class 소트인사이드_review {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String N = br.readLine();
+        String[] arr = N.split("");
+
+        Arrays.sort(arr, Comparator.reverseOrder());
+
+        for(String k : arr){
+            bw.write(k);
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
