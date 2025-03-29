2e8135eeca3e388740eeb095dccd7702f18b2d76
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-03-29 21:19:42 +0900

    add: 심화 2

--- 백준문제/단계별로 풀어보기/심화 2/통계학_review.java
+++ 백준문제/단계별로 풀어보기/심화 2/통계학_review.java
@@ -6,77 +6,81 @@ import static java.lang.Math.round;
 public class 통계학_review {
 
     public static int arithmetic_mean(int[] arr){
+        // 산술평균 : N개의 수들의 합을 N으로 나눈 값(소수점 이하 첫째 자리에서 반올림)
         double result = 0;
         for (int i : arr) {
-            result += i;
+            result +=i;
         }
-
-        return (int) round(result /arr.length);
+        return (int) round(result/arr.length);
     }
 
     public static int median(int[] arr){
-        int result = arr[arr.length/2];
-        return result;
+        // 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+        return arr[arr.length/2];
     }
 
     public static int mode(int[] arr){
+        // 최빈값 : N개의 수들 중 가장 많이 나타나는 값
         HashMap<Integer,Integer> hm = new HashMap<>();
         List<Integer> list = new ArrayList<>();
 
         for (int i : arr) {
             hm.put(i,hm.getOrDefault(i,0)+1);
         }
-        int max = Integer.MIN_VALUE;
-        int maxKey = 0;
-        for (int i : hm.keySet()) {
-            if(max < hm.get(i)){
-                max = hm.get(i);
+
+        int maxValue = Integer.MIN_VALUE;
+        int maxKey;
+
+        for (Integer i : hm.keySet()) {
+            if(hm.get(i) > maxValue){
                 maxKey = i;
+                maxValue = hm.get(i);
             }
         }
 
-        for (int i : hm.keySet()) {
-            if(hm.get(i) == max){
+        for (Integer i : hm.keySet()) {
+            if(hm.get(i) == maxValue){
                 list.add(i);
             }
         }
+
         if(list.size() == 1){
             return list.get(0);
-        } else{
+        } else {
             Collections.sort(list);
             return list.get(1);
         }
     }
 
     public static int range(int[] arr){
-        return arr[arr.length -1] - arr[0];
+        // 범위 : N개의 수들 중 최댓값과 최솟값의 차이
+        return arr[arr.length-1] - arr[0];
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+        int arr[] = new int[N];
 
-        for(int i =0; i<N; i++){
+        for(int i = 0; i<N; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-
         Arrays.sort(arr);
 
-        int arithmetic_mean = arithmetic_mean(arr);
-        int median = median(arr);
-        int mode = mode(arr);
-        int range = range(arr);
-
         StringBuilder sb = new StringBuilder();
-        sb.append(arithmetic_mean).append("\n").append(median).append("\n").append(mode).append("\n").append(range);
+
+        sb.append(arithmetic_mean(arr)).append("\n")
+                .append(median(arr)).append("\n")
+                .append(mode(arr)).append("\n")
+                .append(range(arr)).append("\n");
+
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
+
     }
 
 }
