294fcb4f11c83612a29b024a33f5a39cf76e5c5c
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-04-03 20:09:32 +0900

    add: 재귀

---
+++ 백준문제/단계별로 풀어보기/재귀/재귀의_귀재_review.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class 재귀의_귀재_review {
+    public static int count;
+    public static int isPalindrome(int left,int right, String str){
+        if(left>=right){
+            count++;
+            return 1;
+        }
+
+        if(str.charAt(left) == str.charAt(right)){
+            count++;
+            return isPalindrome(left+1,right-1,str);
+        }else{
+            count++;
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i<T; i++){
+            count = 0;
+            String str = br.readLine();
+            int pal = isPalindrome(0,str.length()-1,str);
+
+            sb.append(pal).append(" ").append(count).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+}
