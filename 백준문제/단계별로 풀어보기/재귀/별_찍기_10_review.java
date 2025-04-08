725ec0c7688dcdd1a217f4dee40a83b8340cb5da
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-04-08 20:10:05 +0900

    add: 재귀

--- 백준문제/단계별로 풀어보기/재귀/별_찍기_10_review.java
+++ 백준문제/단계별로 풀어보기/재귀/별_찍기_10_review.java
@@ -1,2 +1,59 @@
+import java.io.*;
+
 public class 별_찍기_10_review {
+
+    private static String arr[][];
+
+    public static void star(int x, int y, int N, boolean blank){
+
+        if(blank){
+            for(int i = x; i<x+N; i++){
+                for(int j = y; j<y+N; j++){
+                    arr[i][j] = " ";
+                }
+            }
+            return;
+        }
+
+        if(N==1){
+            arr[x][y] = "*";
+            return;
+        }
+
+        int size = N/3;
+        int count = 0;
+        for(int i = x; i<x+N; i+=size){
+            for(int j = y; j<y+N; j+=size){
+                count++;
+                if(count == 5){
+                    star(i,j,size,true);
+                }else{
+                    star(i,j,size,false);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new String[N][N];
+
+        star(0,0,N,false);
+
+        for(int i = 0; i<N; i++){
+            for(int j = 0; j<N; j++){
+                bw.write(arr[i][j]);
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
 }
--- 백준문제/단계별로 풀어보기/재귀/하노이_탑_이동_순서.java
+++ 백준문제/단계별로 풀어보기/재귀/하노이_탑_이동_순서.java
@@ -1,2 +1,36 @@
+import java.io.*;
+
 public class 하노이_탑_이동_순서 {
+
+    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf((int)(Math.pow(2,N) - 1)));
+        bw.write("\n");
+
+        Hanoi(N,1,2,3);
+
+        bw.flush();
+        bw.close();
+
+
+    }
+
+    public static void Hanoi(int N, int start, int mid, int to) throws IOException{
+
+        if(N==1){
+            bw.write(start + " " + to + "\n");
+            return;
+        }
+
+        Hanoi(N-1,start, to, mid);
+
+        bw.write(start + " " + to  + "\n");
+
+        Hanoi(N-1, mid, start, to);
+    }
 }
