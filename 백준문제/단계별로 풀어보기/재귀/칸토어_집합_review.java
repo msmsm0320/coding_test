5b0ba15645e5fc0b364a73929f465350b24ddee8
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-04-10 20:34:08 +0900

    add: 재귀

--- 백준문제/단계별로 풀어보기/재귀/칸토어_집합_review.java
+++ 백준문제/단계별로 풀어보기/재귀/칸토어_집합_review.java
@@ -1,2 +1,47 @@
-public class 칸토어_집합_review {
+import java.io.*;
+import java.util.Scanner
+
+public class 칸토어_집합_review{
+
+    public static String[] arr;
+
+    public static void cantorSet(int start, int len) {
+        if (len==1) return;
+
+        int size = len / 3;
+
+        for (int i = start + size; i<start + 2 * size; i++) {
+            arr[i] = " ";
+        }
+
+        cantorSet(start, size);
+        cantorSet(start + 2 * size, size);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input;
+        while ((input = br.readLine()) != null) {
+            int N = Integer.parseInt(input);
+            int size = (int) Math.pow(3, N);
+            arr = new String[size];
+
+            for (int i = 0; i < size; i++) {
+                arr[i] = "-";
+            }
+
+            cantorSet(0, size);
+
+            for (String str : arr) {
+                bw.write(str);
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
