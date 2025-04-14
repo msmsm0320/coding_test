5c93e3f6c08f5bd40644bf839f4800b48418fd6d
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-04-14 18:49:46 +0900

    Update: 알고리즘_수업_병합_정렬_1

--- 백준문제/단계별로 풀어보기/재귀/알고리즘_수업_병합_정렬_1.java
+++ 백준문제/단계별로 풀어보기/재귀/알고리즘_수업_병합_정렬_1.java
@@ -3,39 +3,42 @@ import java.util.*;
 
 public class 알고리즘_수업_병합_정렬_1 {
     static int[] A, tmp;
-    static int K, count = 0, result = -1;
+    static int N, K, count = 0, result = -1;
 
-    public static void mergeSort(int p, int r) {
-        if (p < r) {
-            int q = (p + r) / 2;
-            mergeSort(p, q);
-            mergeSort(q + 1, r);
-            merge(p, q, r);
+    public static void mergeSort(int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(start, mid);
+            mergeSort(mid + 1, end);
+            merge(start, mid, end);
         }
     }
 
-    public static void merge(int p, int q, int r) {
-        int i = p, j = q + 1, t = 0;
-        while (i <= q && j <= r) {
+    public static void merge(int start, int mid, int end) {
+        int i = start, j = mid + 1, t = start;
+
+        while (i <= mid && j <= end) {
             if (A[i] <= A[j]) {
                 tmp[t++] = A[i++];
             } else {
                 tmp[t++] = A[j++];
             }
         }
-        while (i <= q) {
+
+        while (i <= mid) {
             tmp[t++] = A[i++];
         }
-        while (j <= r) {
+
+        while (j <= end) {
             tmp[t++] = A[j++];
         }
-        i = p;
-        t = 0;
-        while (i <= r) {
-            A[i++] = tmp[t++];
+
+        for (int idx = start; idx <= end; idx++) {
+            A[idx] = tmp[idx];
             count++;
             if (count == K) {
-                result = A[i - 1];
+                result = A[idx];
+                return;
             }
         }
     }
@@ -43,14 +46,18 @@ public class 알고리즘_수업_병합_정렬_1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+
         A = new int[N];
         tmp = new int[N];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
+
         mergeSort(0, N - 1);
         System.out.println(result);
     }
