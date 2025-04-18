a5726c0bd71711cac5be9aa72b33902506b199ae
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-04-18 21:43:38 +0900

    add: 스택 큐 덱

---
+++ 백준문제/단계별로 풀어보기/스택 큐 덱/도키도키_간식드리미_review.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 도키도키_간식드리미_review {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 1;
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (num == count) {
+                count++;
+                // 간식 줄 수 있으면 바로 줌
+                while (!stack.isEmpty() && stack.peek() == count) {
+                    stack.pop();
+                    count++;
+                }
+            } else {
+                stack.push(num);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            bw.write("Nice");
+        } else {
+            bw.write("Sad");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
