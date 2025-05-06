0c6207fcb6bee37e0d1a2f5af51ea7a7ba803df7
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-05-06 21:27:40 +0900

    refactoring : 잃어버린_괄호

--- 백준문제/단계별로 풀어보기/그리디 알고리즘/잃어버린_괄호_review.java
+++ 백준문제/단계별로 풀어보기/그리디 알고리즘/잃어버린_괄호_review.java
@@ -7,7 +7,7 @@ public class 잃어버린_괄호_review {
 
         String input = br.readLine();
 
-        // '-' 기준으로 나누기
+
         String[] minusSplit = input.split("-");
 
         int result = 0;
@@ -16,7 +16,7 @@ public class 잃어버린_괄호_review {
             String[] plusSplit = minusSplit[i].split("\\+");
             int sum = 0;
             for (String s : plusSplit) {
-                sum += Integer.parseInt(s); // 각각 더해줌
+                sum += Integer.parseInt(s);
             }
 
             if (i == 0) {
