f46aea633adbc9845d4d406c0801b537c53d8f48
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-05-08 19:18:58 +0900

    add : 그리디 알고리즘

--- 백준문제/단계별로 풀어보기/그리디 알고리즘/잃어버린_괄호_review.java
+++ 백준문제/단계별로 풀어보기/그리디 알고리즘/잃어버린_괄호_review.java
@@ -1,31 +1,33 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class 잃어버린_괄호_review {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input = br.readLine();
-
-
-        String[] minusSplit = input.split("-");
+        String[] minusSplit = br.readLine().split("-");
 
         int result = 0;
 
-        for (int i = 0; i < minusSplit.length; i++) {
+        for(int i = 0; i< minusSplit.length; i++){
             String[] plusSplit = minusSplit[i].split("\\+");
             int sum = 0;
-            for (String s : plusSplit) {
-                sum += Integer.parseInt(s);
+            for(String num : plusSplit){
+                sum += Integer.parseInt(num);
             }
-
-            if (i == 0) {
+            if(i == 0){
                 result += sum;
             } else {
                 result -= sum;
             }
         }
 
-        System.out.println(result);
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
