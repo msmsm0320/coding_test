32cf8e45815dd9e2a285e1345f5bb9c80a2cbea5
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-05-20 23:55:30 +0900

    add : 동적 계획법1

---
+++ 백준문제/단계별로 풀어보기/동적 계획법1/회의실_배정.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 회의실_배정 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int meeting[][] = new int[N][2];
+
+        StringTokenizer st;
+
+        for(int i = 0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<2; j++){
+                meeting[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        Arrays.sort(meeting,(o1,o2) -> {
+            if(o1[1] == o2[1]) return o1[0]-o2[0];
+            return o1[1] - o2[1];
+        });
+
+
+        int lastFinishedTime = 0;
+        int count = 0;
+
+        for(int i = 0; i<N; i++){
+            if(lastFinishedTime <= meeting[i][0]){
+                count++;
+                lastFinishedTime = meeting[i][1];
+            }
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
