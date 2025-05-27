316c1047024f246f187f65b8dba33bda49e47b54
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-05-27 20:32:17 +0900

    add : 동적 계획법1

---
+++ 백준문제/단계별로 풀어보기/동적 계획법1/RGB거리.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class RGB거리 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] house = new int[N][3];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        house[0][0] = Integer.parseInt(st.nextToken());
+        house[0][1] = Integer.parseInt(st.nextToken());
+        house[0][2] = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j<3; j++){
+                int curCost = Integer.parseInt(st.nextToken());
+
+                switch(j){
+                    case 0 : house[i][0] = Math.min(house[i-1][1] + curCost, house[i-1][2] + curCost);
+                    break;
+                    case 1 : house[i][1] = Math.min(house[i-1][0] + curCost, house[i-1][2] + curCost);
+                    break;
+                    case 2:
+                        house[i][2] = Math.min(house[i-1][1] + curCost, house[i-1][0] + curCost);
+                        break;
+                }
+
+            }
+        }
+        int result = Math.min(house[N-1][0], house[N-1][1]);
+        result = Math.min(result, house[N-1][2]);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
