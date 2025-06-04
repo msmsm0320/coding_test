616c24f952bd2217c018de4d2e97d65975fb4ce3
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-06-05 00:37:34 +0900

    update : 신나는 함수 실행 logic 변경

--- 백준문제/단계별로 풀어보기/동적 계획법1/신나는_함수_실행.java
+++ 백준문제/단계별로 풀어보기/동적 계획법1/신나는_함수_실행.java
@@ -1,55 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class 신나는_함수_실행 {
 
-    static StringBuilder sb = new StringBuilder();
-    static int[][][] me = new int[21][21][21];
+    public static int dp[][][] = new int[21][21][21];
+
+    public static int w(int a, int b, int c){
+
+        if(a >= 0 && a <= 20 && b >= 0 && b <= 20 && c >= 0 && c <= 20 && dp[a][b][c] != 0) return dp[a][b][c];
+
+        if(a <= 0 || b <= 0 || c <= 0) return 1;
+
+        if(a > 20 || b > 20 || c > 20) return dp[20][20][20] = w(20, 20, 20);
+
+        if(a < b && b < c) return dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+
+        return dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+    }
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        while(true) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
+        while(true){
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            if(a==-1 &&b==-1 &&c==-1)
-                break;
+            if(a == -1 && b == -1 && c == -1) break;
 
-            int n = 0;
-            n +=w_function(a,b,c);
 
-            sb.append("w(").append(a).append(", ")
-                    .append(b).append(", ").append(c)
-                    .append(") = ").append(n).append("\n");
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(")").append(" = ").append(w(a,b,c)).append("\n");
+
+
         }
-
-        System.out.println(sb);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
 
     }
-    public static int w_function(int a, int b, int c) {
 
-        if(a>=0&& a<=20&&b>=0&& b<=20&&c>=0&& c<=20&&me[a][b][c]!=0)
-            return me[a][b][c];
-
-        if(a <= 0 || b<=0 || c<=0)
-            return 1;
-
-        if (a>20 || b>20 || c>20)
-            return me[20][20][20]=w_function(20, 20, 20);
-
-        if (a<b&&b<c)
-            return  me[a][b][c]=w_function(a, b, c-1) + w_function(a, b-1, c-1) - w_function(a, b-1, c);
-
-
-        return me[a][b][c]=w_function(a-1, b, c) + w_function(a-1, b-1, c) +
-                w_function(a-1, b, c-1) - w_function(a-1, b-1, c-1);
-    }
 }
