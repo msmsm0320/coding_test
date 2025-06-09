a1fe9359713913fe8a58f3142c45c8ec252cfe31
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-06-09 23:18:13 +0900

    update : 동적 계획법 1

--- 백준문제/단계별로 풀어보기/동적 계획법1/가장_긴_바이토닉_부분_수열.java
+++ 백준문제/단계별로 풀어보기/동적 계획법1/가장_긴_바이토닉_부분_수열.java
@@ -1,56 +1,57 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class 가장_긴_바이토닉_부분_수열 {
 
-    static int N;
-    static int[] seq;
-    static int[] r_dp;
-    static int[] l_dp;
+    public static int[] r_dp;
+    public static int[] l_dp;
+    public static int[] array;
+    public static int N;
 
-    public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-
-        N = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
         r_dp = new int[N];
         l_dp = new int[N];
-        seq = new int[N];
+        array = new int[N];
 
+        Arrays.fill(r_dp, 1);
+        Arrays.fill(l_dp, 1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            seq[i] = in.nextInt();
+            array[i] = Integer.parseInt(st.nextToken());
         }
 
         LIS();
         LDS();
 
         int max = 0;
-        for(int i = 0; i < N; i++) {
-            if(max < r_dp[i] + l_dp[i]) {
-                max = r_dp[i] + l_dp[i];
-            }
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, r_dp[i] + l_dp[i] - 1);
         }
 
-        System.out.println(max - 1);
+        System.out.println(max);
+        br.close();
     }
 
-    static void LIS() {
-        for(int i = 0; i < N; i++) {
-            r_dp[i] = 1;
-            for(int j = 0; j < i; j++) {
-                if(seq[j] < seq[i] && r_dp[i] < r_dp[j] + 1) {
-                    r_dp[i] = r_dp[j] + 1;
+    public static void LIS() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[i]) {
+                    r_dp[i] = Math.max(r_dp[i], r_dp[j] + 1);
                 }
             }
         }
     }
 
-    static void LDS() {
+    public static void LDS() {
         for (int i = N - 1; i >= 0; i--) {
-            l_dp[i] = 1;
             for (int j = N - 1; j > i; j--) {
-                if (seq[j] < seq[i] && l_dp[i] < l_dp[j] + 1) {
-                    l_dp[i] = l_dp[j] + 1;
+                if (array[j] < array[i]) {
+                    l_dp[i] = Math.max(l_dp[i], l_dp[j] + 1);
                 }
             }
         }
