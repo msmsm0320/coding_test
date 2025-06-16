4a6a0d58ae657ee4f2abab16298b0f0523d00064
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-06-16 22:26:29 +0900

    add : 우선순위 큐

--- 백준문제/단계별로 풀어보기/우선순위 큐/최대_힙_review.java
+++ 백준문제/단계별로 풀어보기/우선순위 큐/최대_힙_review.java
@@ -4,27 +4,29 @@ import java.util.PriorityQueue;
 
 public class 최대_힙_review {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i<N; i++){
-            
             int x = Integer.parseInt(br.readLine());
 
             if(x == 0){
-                if(pq.size() == 0){
-                    bw.write(0 + "\n");
+                if(pq.isEmpty()){
+                    sb.append(0).append("\n");
                     continue;
                 }
-                bw.write(pq.poll() + "\n");
-            }else{
-                pq.add(x);
+                sb.append(pq.poll()).append("\n");
+            } else {
+                pq.offer(x);
             }
         }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
