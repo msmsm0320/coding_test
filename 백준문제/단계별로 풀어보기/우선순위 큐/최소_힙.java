0c46ca3f8bef6f1bbea8a8937cad6d9e320241cb
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-06-26 21:52:47 +0900

    add : 우선순위 큐

--- 백준문제/단계별로 풀어보기/우선순위 큐/최소_힙.java
+++ 백준문제/단계별로 풀어보기/우선순위 큐/최소_힙.java
@@ -1,2 +1,26 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
 public class 최소_힙 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> Queue = new PriorityQueue<>();
+
+        while (N-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (Queue.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(Queue.poll()).append("\n");
+                }
+            } else {
+                Queue.add(x);
+            }
+        }
+        br.close();
+        System.out.println(sb);
+    }
 }
