45f4dd3fdc7ff0198b8243b5ede7810f3f385e8d
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-07-02 19:45:05 +0900

    add : 집합과 맵

---
+++ 백준문제/단계별로 풀어보기/집합과 맵/숫자_카드.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class 숫자_카드 {
+
+    public static void main(String[] args) throws IOException {
+
+        HashSet<Integer> cards = new HashSet<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            cards.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        for(int i = 0; i < M; i++){
+            if(cards.contains(Integer.parseInt(st.nextToken()))) sb.append(1).append(" ");
+            else sb.append(0).append(" ");
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+}
