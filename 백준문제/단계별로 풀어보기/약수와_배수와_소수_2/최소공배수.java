a8a5e96cdd56b567624a406029444102b1fa6192
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-07-06 22:30:14 +0900

    add : 약수와_배수와_소수_2

--- 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/최소공배수.java
+++ 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/최소공배수.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class 최소공배수 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+
+
+        for(int i = 0; i < N; i++) {
+
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            int d = gcd(a, b);
+
+            sb.append(a * b / d).append('\n');
+        }
+        System.out.println(sb);
+
+    }
+
+
+    public static int gcd(int a, int b) {
+
+        while (b != 0) {
+            int r = a % b;
+
+            a = b;
+            b = r;
+        }
+        return a;
+    }
 }
