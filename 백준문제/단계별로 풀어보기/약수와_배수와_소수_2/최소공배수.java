70af571de2f3d9a16872697504e9739c83b44dd7
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-07-20 19:30:36 +0900

    add : 약수와_배수와_소수_2

--- 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/최소공배수.java
+++ 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/최소공배수.java
@@ -1,37 +1,42 @@
+import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class 최소공배수 {
-    public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+    public static int gcd(int a, int b){
 
-        int N = in.nextInt();
-
-        StringBuilder sb = new StringBuilder();
-
-
-        for(int i = 0; i < N; i++) {
-
-            int a = in.nextInt();
-            int b = in.nextInt();
-
-            int d = gcd(a, b);
-
-            sb.append(a * b / d).append('\n');
-        }
-        System.out.println(sb);
+        if(b == 0) return a;
+        return gcd(b, a % b);
 
     }
 
+    public static int lcm(int a, int b){
+        return a * (b / gcd(a,b));
+    }
 
-    public static int gcd(int a, int b) {
+    public static void main(String[] args) throws IOException {
 
-        while (b != 0) {
-            int r = a % b;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            a = b;
-            b = r;
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(lcm(a,b)).append("\n");
         }
-        return a;
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }
