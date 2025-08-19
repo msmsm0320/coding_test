27e7f8923a5245197a9292c46ae5c6f3e32dbb36
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-08-19 23:23:22 +0900

    add : 심화 1

--- 백준문제/단계별로 풀어보기/심화_1/별찍기_7.java
+++ 백준문제/단계별로 풀어보기/심화_1/별찍기_7.java
@@ -1,2 +1,28 @@
+import java.io.*;
+
 public class 별찍기_7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 0; j < N-i; j++) sb.append(" ");
+            for(int j = 0; j < 2*i-1; j++) sb.append("*");
+            sb.append("\n");
+        }
+        for(int i = N-1; i >= 1; i--){
+            for(int j = 0; j < N-i; j++) sb.append(" ");
+            for(int j = 0; j < 2*i-1; j++) sb.append("*");
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
