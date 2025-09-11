58ab1170e984d538f033da9706da476e1e61e624
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-09-11 19:33:11 +0900

    add : 집합과 맵

--- 백준문제/단계별로 풀어보기/집합과 맵/문자열_집합.java
+++ 백준문제/단계별로 풀어보기/집합과 맵/문자열_집합.java
@@ -1,2 +1,26 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 public class 문자열_집합 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(br.readLine(), 0);
+        }
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            if (map.containsKey(br.readLine())) count++;
+        }
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
