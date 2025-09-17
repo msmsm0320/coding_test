929ea5d1f378133cd63070606b16673a13c29d35
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-09-17 23:03:52 +0900

    add : 집합과 맵

--- 백준문제/단계별로 풀어보기/집합과 맵/문자열_집합.java
+++ 백준문제/단계별로 풀어보기/집합과 맵/문자열_집합.java
@@ -1,24 +1,32 @@
 import java.io.*;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class 문자열_집합 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        HashMap<String,Integer> hm = new HashMap<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(br.readLine(), 0);
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+
+        for(int i = 0; i < N; i++){
+            String s = br.readLine();
+            hm.put(s,i);
         }
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            if (map.containsKey(br.readLine())) count++;
+
+        for(int i = 0; i < M; i++){
+            String t = br.readLine();
+            if(hm.containsKey(t)) result++;
         }
-        bw.write(String.valueOf(count));
+
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
         br.close();
