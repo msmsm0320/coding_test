0ce3e39f8e01d1a1e2d00000c662e76929e8327a
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-09-18 22:10:00 +0900

    add : 집합과 맵

--- 백준문제/단계별로 풀어보기/집합과 맵/숫자_카드_2.java
+++ 백준문제/단계별로 풀어보기/집합과 맵/숫자_카드_2.java
@@ -1,32 +1,37 @@
 import java.io.*;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class 숫자_카드_2 {
     public static void main(String[] args) throws IOException {
-
-        HashSet<Integer> cards = new HashSet<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
+
+        HashMap<Integer, Integer> cards = new HashMap<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i = 0; i < N; i++){
-            cards.add(Integer.parseInt(st.nextToken()));
+            int card = Integer.parseInt(st.nextToken());
+            cards.put(card, cards.getOrDefault(card, 0)+1);
         }
 
         int M = Integer.parseInt(br.readLine());
+
         st = new StringTokenizer(br.readLine());
+
         StringBuilder sb = new StringBuilder();
 
+        for(int j = 0; j < M; j++){
+            int k = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < M; i++){
-            if(cards.contains(Integer.parseInt(st.nextToken()))) sb.append(1).append(" ");
+            if(cards.get(k) != null) sb.append(cards.get(k)).append(" ");
             else sb.append(0).append(" ");
         }
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write(sb.toString());
         bw.flush();
         bw.close();
