fafb7aef4186d86c1a8dd73d6cd30c25928a4484
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-09-22 22:28:49 +0900

    add : 집합과 맵

--- 백준문제/단계별로 풀어보기/집합과 맵/나는야_포켓몬_마스터_이다솜.java
+++ 백준문제/단계별로 풀어보기/집합과 맵/나는야_포켓몬_마스터_이다솜.java
@@ -1,36 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 public class 나는야_포켓몬_마스터_이다솜 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+
+        HashMap<String, Integer> hm1 = new HashMap<>();
+        HashMap<Integer, String> hm2 = new HashMap<>();
+
+        int count = 1;
+
+        for(int i = 0; i < N; i++){
+            String pocketmon = br.readLine();
+            hm1.put(pocketmon,count);
+            hm2.put(count++,pocketmon);
+        }
+
         StringBuilder sb = new StringBuilder();
 
-        HashMap<Integer, String> hash1 = new HashMap<Integer, String>();
-        HashMap<String, Integer> hash2 = new HashMap<String, Integer>();
+        for(int i = 0; i < M; i++){
+            String pc = br.readLine();
 
-
-        for(int i = 1; i <= N; i++) {
-            String S = br.readLine();
-            hash1.put(i, S);
-            hash2.put(S, i);
-        }
-
-        for(int i = 0; i < M; i++) {
-            String S = br.readLine();
-
-            if(49 <= S.charAt(0) && S.charAt(0) <= 57) {
-                sb.append(hash1.get(Integer.parseInt(S))).append("\n");
-            }else {
-                sb.append(hash2.get(S)).append("\n");
+            if(49 <= pc.charAt(0) && pc.charAt(0) <= 57){
+                sb.append(hm2.get(Integer.parseInt(pc))).append(" ").append("\n");
+            } else{
+                sb.append(hm1.get(pc)).append(" ").append("\n");
             }
         }
-        System.out.println(sb);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
