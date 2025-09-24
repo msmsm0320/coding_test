905a07a787d3fc36d7866e684585f40c907b3c83
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-09-24 23:14:02 +0900

    add : 우선순위 큐

--- 백준문제/단계별로 풀어보기/우선순위 큐/절대값_힙.java
+++ 백준문제/단계별로 풀어보기/우선순위 큐/절대값_힙.java
@@ -1,2 +1,40 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class 절대값_힙 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) ->{
+            if(Math.abs(o1) == Math.abs(o2)){
+                return o1 - o2;
+            }
+            return Math.abs(o1) - Math.abs(o2);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
+            int x = Integer.parseInt(br.readLine());
+
+            if(x == 0){
+                if(pq.size() == 0){
+                    sb.append(0).append("\n");
+                    continue;
+                }
+
+                sb.append(pq.poll()).append("\n");
+            }else{
+                pq.offer(x);
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
--- 백준문제/단계별로 풀어보기/우선순위 큐/최소_힙.java
+++ 백준문제/단계별로 풀어보기/우선순위 큐/최소_힙.java
@@ -4,23 +4,31 @@ import java.util.PriorityQueue;
 public class 최소_힙 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> Queue = new PriorityQueue<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while (N-- > 0) {
-            int x = Integer.parseInt(br.readLine());
-            if (x == 0) {
-                if (Queue.isEmpty()) {
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++){
+            int k = Integer.parseInt(br.readLine());
+
+            if(k == 0){
+                if(pq.size() == 0){
                     sb.append(0).append("\n");
-                } else {
-                    sb.append(Queue.poll()).append("\n");
+                    continue;
                 }
+                sb.append(pq.poll()).append("\n");
             } else {
-                Queue.add(x);
+                pq.offer(k);
             }
         }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
         br.close();
-        System.out.println(sb);
     }
 }
