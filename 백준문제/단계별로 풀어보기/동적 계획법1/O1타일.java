0f938096cbfaf09c00c3b493e05ac58c7b272670
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-09-25 23:25:39 +0900

    add : 동적 계획법1

---
+++ 백준문제/단계별로 풀어보기/동적 계획법1/O1타일.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class O1타일 {
+
+    public static int dp[];
+
+
+    public static int fib(int n){
+
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2])%15746;
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        dp = new int[N+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        int result = fib(N);
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
