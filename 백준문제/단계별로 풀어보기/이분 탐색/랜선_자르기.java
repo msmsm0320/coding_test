af3f7769e00d58144976c23b05cb7ee3feff3b0c
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-09-26 12:21:41 +0900

    add : 이분 탐색

---
+++ 백준문제/단계별로 풀어보기/이분 탐색/랜선_자르기.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class 랜선_자르기 {
+
+    public static List<Long> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long K = Long.parseLong(st.nextToken());
+        long N = Long.parseLong(st.nextToken());
+
+        long max = 0;
+        for(int i = 0; i < K; i++){
+            long x = Long.parseLong(br.readLine());
+            list.add(x);
+            if(x > max) max = x;
+
+        }
+
+        max++;
+
+        long min = 1;
+        long mid = 0;
+
+        while(min < max) {
+
+            mid = (max + min)/2;
+
+            long count = 0;
+
+            for(int i = 0; i < list.size(); i++){
+                count += list.get(i)/mid;
+            }
+
+            if(count < N){
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        bw.write(String.valueOf(min-1));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
