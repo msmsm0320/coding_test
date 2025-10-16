0d9d639d90eb1b5722f3133ae130608bba4ab560
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-10-16 16:53:07 +0900

    add : 그래프와 순회

--- 백준문제/단계별로 풀어보기/그래프와 순회/뱀과_사다리_게임.java
+++ 백준문제/단계별로 풀어보기/그래프와 순회/뱀과_사다리_게임.java
@@ -1,6 +1,56 @@
-public class 뱀과_사다리_게임 {
-<<<<<<< HEAD
-=======
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
->>>>>>> 5b18ac3bd0614d90a6652ea184782e252ab738b2
+public class 뱀과_사다리_게임 {
+    static int arr[],N,M;
+    static boolean visit[];
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[101];
+        visit = new boolean[101];
+
+        for(int i=0; i<N+M; i++){
+            st = new StringTokenizer(br.readLine());
+            int x =Integer.parseInt(st.nextToken());
+            int y =Integer.parseInt(st.nextToken());
+            arr[x]=y;
+        }
+
+        bfs(1);
+    }
+    static void bfs(int start){
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int[]{start,0});
+        visit[start]=true;
+
+        while(!q.isEmpty()){
+            int p[] = q.poll();
+
+            if(p[0]==100){
+                System.out.println(p[1]);
+                return;
+            }
+
+            for(int i=1; i<7; i++){
+                int next = p[0]+i;
+                if(next<=100){
+                    if(arr[next]!=0){
+                        next = arr[next];
+                    }
+                    if(!visit[next]){
+                        visit[next] = true;
+                        q.add(new int[]{next,p[1]+1});
+
+                    }
+                }
+            }
+        }
+    }
 }
