e4b749a4bfe8f3c47271c0779882c9ef8a2c805d
msmsm0320 <114889106+msmsm0320@users.noreply.github.com>
2025-10-21 22:26:08 +0900

    add : 약수와 배수와 소수 2

--- 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/다음_소수.java
+++ 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/다음_소수.java
@@ -1,2 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 다음_소수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i =0; i< T; i++){
+            long N = Long.parseLong(br.readLine());
+            if (N <= 1) N = 2;
+            while(true){
+                int count = 0;
+                for(int j = 2; j<=Math.sqrt(N); j++){
+                    if(N%j==0){
+                        count++;
+                        break;
+                    }
+                }
+
+                if(count == 0){
+                    System.out.println(N);
+                    break;
+                }
+                N++;
+            }
+        }
+    }
 }
