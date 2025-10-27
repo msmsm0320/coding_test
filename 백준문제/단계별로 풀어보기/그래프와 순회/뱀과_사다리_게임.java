07dd94c97efd7d7e6ff551598406bd9054a53f60
msmsm0320 <ms000320@naver.com>
2025-10-27 22:42:14 +0900

    add : 그래프와 순회

--- 백준문제/단계별로 풀어보기/그래프와 순회/뱀과_사다리_게임.java
+++ 백준문제/단계별로 풀어보기/그래프와 순회/뱀과_사다리_게임.java
@@ -1,56 +1,69 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 뱀과_사다리_게임 {
-    static int arr[],N,M;
-    static boolean visit[];
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[101];
-        visit = new boolean[101];
 
-        for(int i=0; i<N+M; i++){
-            st = new StringTokenizer(br.readLine());
-            int x =Integer.parseInt(st.nextToken());
-            int y =Integer.parseInt(st.nextToken());
-            arr[x]=y;
-        }
+    public static int N,M;
+    public static int[] arr = new int[101];
+    public static boolean[] visited = new boolean[101];
 
-        bfs(1);
-    }
-    static void bfs(int start){
-        Queue<int[]> q=new LinkedList<>();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void bfs(int start) throws IOException {
+        Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{start,0});
-        visit[start]=true;
 
-        while(!q.isEmpty()){
-            int p[] = q.poll();
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
 
-            if(p[0]==100){
-                System.out.println(p[1]);
+            if(cur[0] == 100){
+                bw.write(String.valueOf(cur[1]));
+                bw.flush();
+                bw.close();
                 return;
             }
 
-            for(int i=1; i<7; i++){
-                int next = p[0]+i;
+            for(int i = 1; i < 7; i++){
+                int next = cur[0] + i;
                 if(next<=100){
                     if(arr[next]!=0){
                         next = arr[next];
                     }
-                    if(!visit[next]){
-                        visit[next] = true;
-                        q.add(new int[]{next,p[1]+1});
-
+                    if(!visited[next]){
+                        visited[next] = true;
+                        q.add(new int[]{next,cur[1]+1});
                     }
                 }
             }
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < N + M; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[x] = y;
+        }
+
+        bfs(1);
+
+
+        br.close();
+
+    }
+
 }
