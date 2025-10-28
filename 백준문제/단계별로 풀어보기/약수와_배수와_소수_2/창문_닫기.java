71f3a3fae40d0fcb43759a074e9f176fb118d496
msmsm0320 <ms000320@naver.com>
2025-10-28 21:59:09 +0900

    add : 약수와 배수와 소수 2

--- 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/창문_닫기.java
+++ 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/창문_닫기.java
@@ -1,3 +1,25 @@
-public class 창문_닫기 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
+public class 창문_닫기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0; //창문이 열려있는 개수
+        for(int i = 1; i * i <= N; i++) { //제곱수만 카운트
+            count++;
+        }
+
+        bw.write(count + "\n");
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
 }
