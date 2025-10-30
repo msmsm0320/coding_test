73ceaa6c522acefcf77e8a77bc8ddb39326b87a5
msmsm0320 <ms000320@naver.com>
2025-10-30 20:35:06 +0900

    add : 약수와 배수와 소수 2

--- 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/분수_합.java
+++ 백준문제/단계별로 풀어보기/약수와_배수와_소수_2/분수_합.java
@@ -2,43 +2,37 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class 분수_합 {
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int C = Integer.parseInt(st.nextToken());
-        int D = Integer.parseInt(st.nextToken());
 
-        int numerator = A * D + B * C;
-        int denominator = B * D;
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
 
-        int mod = gcd(numerator, denominator);
-        numerator /= mod;
-        denominator /= mod;
+        int m = (a * d) + (b * c); //분자
+        int n = b * d; //분모
 
-        bw.write(numerator + " " + denominator + "\n");
+        int k = 0;
+        if(m > n) k = gcd(m, n);
+        else k = gcd(n, m);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(m/k).append(" ").append(n/k);
+
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
-
-
-    public static int gcd(int a, int b) {
-
-        if (a <= b) {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b , a % b);
-    }
-
 }
